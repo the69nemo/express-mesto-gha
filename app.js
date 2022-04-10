1e5be5171ad79acf661f341f68ce595fdcdadcff
the69nemo <thestigmata@yandex.ru>
2022-04-10 21:57:05 +0300

    PR13: page404

--- app.js
+++ app.js
@@ -21,5 +21,8 @@ app.use((req, res, next) => {
 
 app.use('/users', require('./routes/user'));
 app.use('/cards', require('./routes/card'));
+app.use((req, res) =>{
+  res.status(404).send({ message: 'Запрашиваемая страница отсутсвует'})
+})
 
 app.listen(PORT);
